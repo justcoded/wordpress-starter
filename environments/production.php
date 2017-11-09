3cbec29f42f009a4ac916bca99862f02c0748736
alex_prokopenko <ab.prokopenko@gmail.com>
2017-11-09 11:57:20 +0200

    Starter base files

---
+++ environments/production.php
@@ -0,0 +1,8 @@
+<?php
+/** Production */
+ini_set( 'display_errors', 0 );
+define( 'WP_DEBUG_DISPLAY', false );
+define( 'SCRIPT_DEBUG', false );
+/** Disable all file modifications including updates and update notifications */
+define( 'DISALLOW_FILE_EDIT', true );
+define( 'DISALLOW_FILE_MODS', true );
---
+++ wp-config.php
@@ -0,0 +1,66 @@
+<?php
+
+$root_dir = dirname( __FILE__ );
+
+require $root_dir . '/environments/dotenv.php';
+
+/**
+ * Use Dotenv to set required environment variables and load .env file in root
+ */
+$dotenv = dotenv( $root_dir );
+$dotenv->load();
+$dotenv->required( [ 'DB_NAME', 'DB_USER', 'DB_PASSWORD', 'WP_HOME', 'WP_SITEURL' ] );
+
+/**
+ * Set up our global environment constant and load its config first
+ * Default: production
+ */
+define( 'WP_ENV', env( 'WP_ENV', 'production' ) );
+require_once $root_dir . '/environments/' . WP_ENV . '.php';
+
+/**
+ * URLs
+ */
+define( 'WP_HOME', env( 'WP_HOME' ) );
+define( 'WP_SITEURL', env( 'WP_SITEURL' ) );
+
+/**
+ * Custom Content Directory
+ */
+define( 'CONTENT_DIR', '/wp-content' );
+define( 'WP_CONTENT_DIR', $root_dir . CONTENT_DIR );
+define( 'WP_CONTENT_URL', WP_HOME . CONTENT_DIR );
+
+/**
+ * DB settings
+ */
+define( 'DB_NAME', env( 'DB_NAME' ) );
+define( 'DB_USER', env( 'DB_USER' ) );
+define( 'DB_PASSWORD', env( 'DB_PASSWORD' ) );
+define( 'DB_HOST', env( 'DB_HOST', '127.0.0.1' ) );
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
+$table_prefix = env( 'DB_PREFIX', 'wp_' );
+
+/**
+ * Authentication Unique Keys and Salts
+ */
+define( 'AUTH_KEY', env( 'AUTH_KEY' ) );
+define( 'SECURE_AUTH_KEY', env( 'SECURE_AUTH_KEY' ) );
+define( 'LOGGED_IN_KEY', env( 'LOGGED_IN_KEY' ) );
+define( 'NONCE_KEY', env( 'NONCE_KEY' ) );
+define( 'AUTH_SALT', env( 'AUTH_SALT' ) );
+define( 'SECURE_AUTH_SALT', env( 'SECURE_AUTH_SALT' ) );
+define( 'LOGGED_IN_SALT', env( 'LOGGED_IN_SALT' ) );
+define( 'NONCE_SALT', env( 'NONCE_SALT' ) );
+
+/**
+ * Custom Settings
+ */
+define( 'AUTOMATIC_UPDATER_DISABLED', true );
+define( 'DISABLE_WP_CRON', env( 'DISABLE_WP_CRON', false ) );
+
+/**
+ * Sets up WordPress vars and included files.
+ */
+require_once( ABSPATH . 'wp-settings.php' );
