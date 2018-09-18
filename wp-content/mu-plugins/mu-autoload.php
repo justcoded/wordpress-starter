46ee667f3f5948b1e2a8cb8f17babbc2b8bffa8c
Alex Prokopenko <ab.prokopenko@gmail.com>
2018-09-18 18:03:45 +0300

    mu-autoloader add support to map custom paths

--- wp-content/mu-plugins/mu-autoload.php
+++ wp-content/mu-plugins/mu-autoload.php
@@ -4,12 +4,22 @@ if ( ( defined( 'WP_INSTALLING' ) && WP_INSTALLING ) || ! $h = opendir( WPMU_PLU
 	return;
 }
 
+$mu_plugins_map = [
+	'advanced-custom-fields'     => 'acf.php',
+	'advanced-custom-fields-pro' => 'acf.php',
+];
+
 while ( false !== ( $entry = readdir( $h ) ) ) {
 	if ( '.' === $entry || '..' === $entry || ! is_dir( WPMU_PLUGIN_DIR . '/' . $entry ) ) {
 		continue;
 	}
 
-	$plugin_file = WPMU_PLUGIN_DIR . '/' . $entry . '/' . $entry . '.php';
+	$filename = $entry . '.php';
+	if ( isset( $mu_plugins_map[ $entry ] ) ) {
+		$filename = $mu_plugins_map[ $entry ];
+	}
+
+	$plugin_file = WPMU_PLUGIN_DIR . '/' . $entry . '/' . $filename;
 	if ( is_file( $plugin_file ) ) {
 		require_once $plugin_file;
 	}
